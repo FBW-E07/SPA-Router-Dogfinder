f581fcdfb22b794e0708083ad5c284e586edba72
nrcool <nrcoolove@yahoo.com>
2021-03-26 15:26:53 +0100

    added solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Dogs from './components/Dogs';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          {/* Render the Dogs component for all the dogs */}
+          <Route exact path="/dogs" component={Dogs} />
+          {/* Render the Dogs component for a specific dog */}
+          <Route exact path="/dogs/:name" component={Dogs} />
+          <Redirect to="/dogs" />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
-import React from "react"
-import App from "./App.js"
-import reactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-reactDOM.render(<App/>,document.getElementById("root"))
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
